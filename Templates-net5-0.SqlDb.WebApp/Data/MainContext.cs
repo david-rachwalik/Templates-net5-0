0d2eb479e4a8cd93f9454c7caff4637f92880c68
David Rachwalik <david.rachwalik@outlook.com>
2021-03-09 16:14:16 -0600

    initial SQL template from Tutorials-net5-0-WebApps-RazorPages

---
+++ Templates-net5-0.SqlDb.WebApp/Data/MainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Templates_net5_0.SqlDb.WebApp.Models;
+
+namespace Templates_net5_0.SqlDb.WebApp.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext (DbContextOptions<MainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Templates-net5-0.SqlDb.WebApp/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Templates_net5_0.SqlDb.WebApp.Data;
+using Templates_net5_0.SqlDb.WebApp.Models;
+
+namespace Templates_net5_0.SqlDb.WebApp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Templates_net5_0.SqlDb.WebApp.Data.MainContext _context;
+
+        public CreateModel(Templates_net5_0.SqlDb.WebApp.Data.MainContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
